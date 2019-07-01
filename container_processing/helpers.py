7ca177329aea7919fd9aeb0a8a6024879f86d39a
Jon Schlueter <jschluet@redhat.com>
2019-07-01 05:55:26 -0400

    Another round of cleanup and normalization

--- container_processing/helpers.py
+++ container_processing/helpers.py
@@ -1,18 +1,8 @@
 from koji_wrapper.tag import KojiTag
 
 
-def get_tags_from_build(koji_session, nvr):
-    builddata = koji_session.build(nvr)
-    return builddata['extra']['image']['index']['tags']
-
-
-def get_parent_nvrs(koji_session, nvr):
-    builddata = koji_session.session.getBuild(nvr)
-    return [i['nvr'] for i in builddata['extra']['image']['partent_image_builds']['tags'].values()]
-
-
 # Search for batch for a tag (will not pick up isolated builds)
-def get_matching_batch_from_tag(osp, batch, rhel, latest=False, koji_session=None, sub_tag='candidate'):
+def get_matching_batch_from_tag(osp, rhel, batch, latest=False, koji_session=None, sub_tag='candidate'):
     tag = "rhos-{0}-rhel-{1}-{2}".format(float(osp), int(rhel), sub_tag)
     if koji_session:
         koji_tag = KojiTag(session=koji_session, tag=tag)
@@ -31,41 +21,63 @@ def get_matching_batch_from_koji_tag(koji_tag, batch):
         if '-container' not in build['package_name']:
             continue
 
-        nvr = build['nvr']
-        component = build['package_name']
-        builddata = koji_tag.session.getBuild(build['id'])
-        tags = builddata['extra']['image']['index']['tags']
-        if [i for i in tags if batch in i]:
+        record = getRecordForBuild(koji_tag, build['id'], grab_build_task_info=True)
+        component = record['package_name']
+
+        if [i for i in record['task_pullspecs'] if batch in i]:
             if component not in matching_containers:
                 matching_containers[component] = []
-            matching_containers[component].append(nvr)
+            matching_containers[component].append(record)
 
     return matching_containers
 
 
-def get_latest_cdn_containers(osp, rhel, koji_session=None):
+def get_latest_cdn_containers(osp, rhel, koji_session=None, latest=True, extra_info=False):
     tag = "rhos-{0}-rhel-{1}-container-released".format(float(osp), int(rhel))
     if koji_session:
         koji_tag = KojiTag(session=koji_session, tag=tag)
     else:
         koji_tag = KojiTag(profile='brew', tag=tag)
 
-    koji_tag.builds(latest=True, type='image', inherit=False)
+    koji_tag.builds(latest=latest, type='image', inherit=False)
 
-    return get_latest_from_koji_tag(koji_tag)
+    return get_container_builds_from_koji_tag(koji_tag, get_extra_info=extra_info)
 
 
-def get_latest_from_koji_tag(koji_tag):
+def getRecordForBuild(koji_wrapper, build_id_or_nvr, grab_build_task_info=False):
+    builddata = koji_wrapper.session.getBuild(build_id_or_nvr)
+    parent_build_id = builddata['extra']['image']['parent_build_id']
+
+    # Useful information but does not include floating tags
+    # pullspecs = builddata['extra']['image']['index']['pull']
+    # tags = builddata['extra']['image']['index']['tags']
+    build_task_id = builddata['extra']['container_koji_task_id']
+    task_results = koji_wrapper.session.getTaskResult(build_task_id)
+    pullspecs = task_results['repositories']
+
+    # tags from builddata will be only non floating tags
+    # pullsepcs from builddata will only be main :{ver}-{rel}
+    return {
+        'package_name': builddata['package_name'],
+        'nvr': builddata['nvr'],
+        'build_id': builddata['id'],
+        'build_pullspecs': builddata['extra']['image']['index']['pull'],
+        'build_tags': builddata['extra']['image']['index']['tags'],
+        'task_pullspecs': pullspecs,
+        'parent_build_id': parent_build_id
+    }
+
+
+def get_container_builds_from_koji_tag(koji_tag, get_extra_info=False):
     matching_containers = {}
 
     for build in koji_tag.builds():
-        nvr = build['nvr']
         component = build['package_name']
 
         if component not in matching_containers:
             matching_containers[component] = []
-        matching_containers[component].append(nvr)
+
+        record = getRecordForBuild(koji_tag, build['id'], grab_build_task_info=False)
+        matching_containers[component].append(record)
 
     return matching_containers
-
-
